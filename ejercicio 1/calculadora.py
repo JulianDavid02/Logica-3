a346a67b54feba6304e10293cafdf51e5ad070b8
JulianDavid02 <juliantec1995@gmail.com>
2024-01-20 11:52:12 -0500

    Correción y explicación

--- ejercicio 1/calculadora.py
+++ ejercicio 1/calculadora.py
@@ -1,23 +1,34 @@
+#Explicación
+
+#Se establece input en los float para que permitiera realizar la operación al momento de ingresar los símbolos.
+# Se corrige las variables num ya que al faltarle un letra - número, el código al momento de compilar no lo encontraba.
+#Se agrega return para que devolviera el resultado
+
+
+
 def calculadora():
-    num1 = float(input("Ingrese el primer número: "))
-    num2 = float(input("Ingrese el segundo número: "))
+    num1 = float(input("Ingrese el primer número: ")) # Se agrega input en los float
+    num2 = float(input("Ingrese el segundo número: ")) # Se agrega input en los float
     operacion = input ("Ingrese la operación (+, -, *, /): ")
     
     if operacion == '+':
         resultado = num1 + num2
     elif operacion == '-':
-        resultado = num1 - num2
+        resultado = num1 - num2 # Se corrige las variables num
     elif operacion == '*':
-        resultado = num1 * num2
+        resultado = num1 * num2 # Se corrige las variables num
     elif operacion == '/':
         resultado = num1 / num2
     else:
         resultado = "Operación no válida"
 
-    return print(resultado)
+    return print(resultado) # Se agrega return en el print y se elimina el segundo texto "resultado"
 
-calculadora()
+calculadora() # La palabra calculadora al final estaba mal escrita.
 
-# Se agrega return, input en los float
+
+
+
+
 
 
