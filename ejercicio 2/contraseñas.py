a2964de0eddc518521f924d9cce1e86eec223807
JulianDavid02 <juliantec1995@gmail.com>
2024-01-27 09:18:00 -0500

    Solución hasta el 4to

--- ejercicio 2/contraseñas.py
+++ ejercicio 2/contraseñas.py
@@ -1,9 +1,17 @@
-import rando
+#Explicación
+
+#Con la función random lo que hacemos es llamar caracteres aleatorios, con la cantidad le decimos que sean 8 y lo que hace es combinarlos al azar.
+
+import random #Se corrige la palabra Random ya que le faltaba una letra
 import string
 
-generar_contraseña(longitud=8):
+def generar_contraseña(longitud=8): #Se define la función ya que le faltaba el DEF
     caracteres = string.ascii_letters + string.digits + string.punctuation
     contraseña = ''.join(random.choice(caracteres) for _ in range(longitud))
-    retunr contraseña
+    return contraseña
 
-print(generar_contrasena())
+contraseña_generada = generar_contraseña(8) 
+
+
+print(f" La contraseña es: {contraseña_generada}") #Se agrega esta parte para que al momento de imprimir nos indique el texto completo 
+
--- ejercicio 4/temperatura.py
+++ ejercicio 4/temperatura.py
@@ -1,6 +1,8 @@
-def celsius_a_fahrenheit(celsius)
-    return (celsius * 9/5)  32
+#Explicación
+#Lo que hace el código es pedirnos ingresar la temperatura en celsius y con la fórmula matemática que corregimos lo convierte a Fahrenheit
+def celsius_a_fahrenheit(celsius): #Se agregan los dos puntos al final
+    return (celsius * 9/5) + 32 #Se agrega el + para que la fórmula funcione
 
-temperatura_celsius = float("Ingrese la temperatura en Celsius: ")
+temperatura_celsius = float(input("Ingrese la temperatura en Celsius: ")) #Se agrega el input para que nos pida ingresar el texto
 temperatura_fahrenheit = celsius_a_fahrenheit(temperatura_celsius)
 print(f"{temperatura_celsius}°C es igual a {temperatura_fahrenheit}°F")
